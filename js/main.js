607c7ab38a712d2d93cde91be19bac846f276ffd
Pedro Diniz <pedrogdiniz@gmail.com>
2021-01-03 15:24:28 -0300

    aula 1 terminada

---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+*/
+/*
+var coount;
+for(count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+/*
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    count++;
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade")
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade")
+};
+*/
+
+
+//var frutas = [{nome:"maca", cor:"vermelha"},{nome:"uva", cor:"roxa"}];
+//console.log(frutas);
+///alert(frutas[1].nome);
+
+//var fruta = {nome:"maca", cor:"vermelha"};
+//console.log(fruta);
+//console.log(fruta.nome);
+//console.log(fruta.cor);
+//alert(fruta.cor);
+
+//var lista = ["maca", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop()
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(" - "))
+
+
+//var nome = "Pedro Diniz"
+//var idade = 34;
+//var idade2 = 10;
+//var frase = " Djokovic Goat"
+// alert(nome + " tem" + idade + " idade");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace(" Djokovic", "Federer"))
+
